75ba625dd8cc50ec713da29138c41ec3ad72c493
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-04-16 19:27:56 +0700

    Initial Project SpringBoot

---
+++ src/test/java/id/ac/ui/cs/advprog/subsmanagementservice/SubsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subsmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
