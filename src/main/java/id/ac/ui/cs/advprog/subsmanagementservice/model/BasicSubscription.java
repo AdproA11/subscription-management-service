0906c15e9a9697273a4c10a6881be3a482eaf22f
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-04-24 21:03:05 +0700

    [GREEN] Implementation For Quarterly and SemiAnnual Subscription Model for Decorator Pattern

--- src/main/java/id/ac/ui/cs/advprog/subsmanagementservice/model/BasicSubscription.java
+++ src/main/java/id/ac/ui/cs/advprog/subsmanagementservice/model/BasicSubscription.java
@@ -1,2 +1,23 @@
-package id.ac.ui.cs.advprog.subsmanagementservice.model;public class BasicSubscription {
+package id.ac.ui.cs.advprog.subsmanagementservice.model;
+
+public class BasicSubscription implements Subscription {
+    private String id;
+    private double price;
+
+    public BasicSubscription(String id, double price) {
+        this.id = id;
+        this.price = price;
+    }
+    @Override
+    public String getId() {
+        return id;
+    }
+    @Override
+    public String getDescription() {
+        return "";
+    }
+    @Override
+    public double getPrice() {
+        return price;
+    }
 }
