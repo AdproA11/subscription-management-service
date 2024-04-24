838d7c54b49518e633280950d848c3677d48fdf5
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-04-24 20:49:41 +0700

    [GREEN] Implementation SubscriptionBox Model

--- src/main/java/id/ac/ui/cs/advprog/subsmanagementservice/model/SubscriptionBox.java
+++ src/main/java/id/ac/ui/cs/advprog/subsmanagementservice/model/SubscriptionBox.java
@@ -1,4 +1,52 @@
 package id.ac.ui.cs.advprog.subsmanagementservice.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subscription_box")
 public class SubscriptionBox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name= "name",nullable = false)
+    private String name;
+
+    @Column(name= "price",nullable = false)
+    private double price;
+
+    // tambahin Constructors, getters, and setters
+    // Constructors
+    public SubscriptionBox() {
+    }
+
+    public SubscriptionBox(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
