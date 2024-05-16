3aa40cbacf3bd336d5fa57ec3790adceaaeddafa
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-05-16 11:07:34 +0700

    [REFACTOR] Add Homepage Static

---
+++ src/main/java/id/ac/ui/cs/advprog/subsmanagementservice/controller/HomepageController.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.subsmanagementservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomepageController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> homepage() {
+        return ResponseEntity.ok("homepage");
+    }
+
+}
